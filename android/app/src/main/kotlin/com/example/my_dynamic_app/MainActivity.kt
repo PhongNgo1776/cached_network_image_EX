b39eff50f73119c016dc4ad394be82387adbb3e2
PhongNgo <ptngo@akvagroup.com>
2022-06-19 20:19:26 +0700

    cached network image example

---
+++ android/app/src/main/kotlin/com/example/my_dynamic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_dynamic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/cached_network_image/cached_network_image.dart
@@ -0,0 +1,7 @@
+// library cached_network_image;
+
+// export 'package:flutter_cache_manager/flutter_cache_manager.dart'
+//     show CacheManagerLogLevel, DownloadProgress;
+// export 'image_provider/cached_image_widget.dart';
+// export 'image_provider/cached_network_image_provider.dart';
+// export 'image_provider/multi_image_stream_completer.dart';
